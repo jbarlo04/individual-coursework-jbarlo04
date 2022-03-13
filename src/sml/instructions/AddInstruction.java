b11c81be8c933958ce358bb3d9ad3cff038ec8f9
Juris Barlots <jbarlo04@student.bbk.ac.uk>
2022-03-13 13:26:47 +0000

    addInstruction class implementation

--- src/sml/instructions/AddInstruction.java
+++ src/sml/instructions/AddInstruction.java
@@ -2,6 +2,7 @@ package sml.instructions;
 
 import sml.Instruction;
 import sml.Machine;
+import sml.Registers;
 
 /**
  * This class represents the Add instruction from the language.
@@ -10,12 +11,20 @@ import sml.Machine;
  */
 public class AddInstruction extends Instruction {
 
+    private int register;
+    private int s1;
+    private int s2;
+
     public AddInstruction(String label, int register, int s1, int s2){
-        super("Something", "Something Else");
+        super(label, "add");
+        this.register = register;
+        this.s1 = s1;
+        this.s2 = s2;
     }
 
     @Override
     public void execute(Machine m) {
-        // TODO: Implement
+        Registers regs = m.getRegisters();
+        regs.setRegister(register, regs.getRegister(s1) + regs.getRegister(s2));
     }
 }
